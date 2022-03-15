82eb8787c2720d9f90d176625548c617a50c13b1
LuisSigala <a6519150031@utch.edu.mx>
2022-03-14 20:42:43 -0700

    Roberto's Ionic Project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiconsumerService } from '../services/apiconsumer.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
 
-  constructor() {}
+  response;
 
+  public title = "BIS";
+  canUSeeMe = true;
+  myCardsArray = [
+    {id:15, name:'megaman'}, 
+    {id:12, name:'protoman'},
+    {id:13, name:'elecman'},
+    {id:11, name:'gutsman'},
+    {id:15, name:'coldman'}
+  ]
+
+  constructor(private apiconsumer: ApiconsumerService) {}
+
+  changetitle(){
+    this.title = "Bye";
+  }
+  sayHello(){
+    this.canUSeeMe = !this.canUSeeMe;
+  }
+
+  CallAPI(){
+    this.apiconsumer.test().subscribe((response) => {
+      let localResponse;
+      localResponse = response;
+    });
+  };
 }
+
+//FormsModule
+//ReactiveFormsModule
